6a4b6a7b841eb9e8f8ee5dd88af22395b64eeb6d
thomas <thomas.chalanson@sfr.fr>
2021-12-06 12:16:03 +0100

    add a form

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,19 +2,22 @@ import React from "react";
 import "./Search.css";
 
 function Search() {
-
   const { Api_key } = process.env;
 
   return (
     <>
       <div className="search">
-        <input
-          type="text"
-          name="search"
-          id="searchBar"
-          className="search-bar"
-          placeholder="Lyon"
-        />
+        <form>
+          <input
+            type="text"
+            name="search"
+            id="searchBar"
+            className="search-bar"
+            placeholder="Lyon"
+          />
+
+          <input type="submit" value="Submit" className="submit" />
+        </form>
       </div>
     </>
   );
